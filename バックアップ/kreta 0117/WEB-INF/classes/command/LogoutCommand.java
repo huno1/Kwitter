4bf0d236c41b715ed128a14a35e0a1af62bf9b0c
H16304004\daisuke <do16304004@ga.tera-house.ac.jp>
2018-01-23 16:05:24 +0900

    表定義微修正

--- バックアップ/kreta 0117/WEB-INF/classes/command/LogoutCommand.java
+++ バックアップ/kreta 0117/WEB-INF/classes/command/LogoutCommand.java
@@ -6,12 +6,12 @@ import javax.servlet.http.HttpSession;
 public class LogoutCommand extends AbstractCommand{
   public ResponseContext execute(ResponseContext resc){
     RequestContext reqc=getRequestContext();
-  	
+
   	//WEB‚Ì‚è‚ª‚·‚é‚Ì‚Í¡‚Í•Û—¯
   	HttpSession session = reqc.getSession();
   	session.invalidate();
 
-    resc.setTarget("start");
+    resc.setTarget("logout");
     return resc;
   }
 }
