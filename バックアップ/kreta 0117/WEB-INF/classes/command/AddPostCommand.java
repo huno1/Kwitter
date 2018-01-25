9e6d7e6a0b940291b88161376e3a76fbf6ebaf1c
H16304004\daisuke <do16304004@ga.tera-house.ac.jp>
2018-01-25 13:34:07 +0900

    a

--- バックアップ/kreta 0117/WEB-INF/classes/command/AddPostCommand.java
+++ バックアップ/kreta 0117/WEB-INF/classes/command/AddPostCommand.java
@@ -1,58 +1,47 @@
 package command;
 
-import tera.RequestContext;
-import tera.ResponseContext;
-import beans.PostBean;
-
-import dao.OracleConnectionManager;
-import dao.AbstractDaoFactory;
-import dao.ProductsDao;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-public class AddPostCommand extends AbstractCommand{
+class AddPostCommand extends AbstractCommand{
 
     public ResponseContext execute(ResponseContext resc){
-    //   RequestContext reqc = getRequestContext();
-    //
-    //   String[] postContent = reqc.getParameter("postcontent");
-    //
-    //   PostBean pb = new PostBean();
-    //
-    //   pb.setPPostContent(postContent[0]);
-    //
-    //   OracleConnectionManager ocm = OracleConnectionManager.getInstance();
-  	// 	ocm.beginTransaction();
-    //
-    //   Connection cn = null;
-    //   PreparedStatement st = null;
-    //
-    //   try{
-    //
-    //     cn = OracleConnectionManager.getInstance().getConnection();
-    //
-    //     String sql = "insert into Post(pPostId ,pPostContent) values(postid_seq.nextval,?)";
-    //
-    //     st = cn.prepareStatement(sql);
-    //
-    //     st.setString(1, pb.getPPostID());
-    //     st.executeUpdate();
-    //
-    //   }catch(Exception e){
-    //   e.printStackTrace();
-    //   }finally{
-    //
-    // }
-    //
-    //   AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
-    //
-  	// 	ProductsDao dao = factory.getProductsDao();
-    //
-  	// 	ocm.commit();
-  	// 	ocm.closeConnection();
+      RequestContext reqc = getRequestContext();
 
-        resc.setTarget("addpost");
+      String[] postContent = reqc.getParameter("postcontent");
+
+      PostBean pb = new PostBean();
+
+      pb.setPPostContent(postContent[0]);
+
+      OracleConnectionManager ocm = OracleConnectionManager.getInstance();
+  		ocm.beginTransaction();
+
+      Connection cn = null;
+      PreparedStatement st = null;
+
+      try{
+
+        cn = OracleConnectionManager.getInstance().getConnection();
+
+        String sql = "insert into Post(pPostId ,pPostContent) values(postid_seq.nextval,?)";
+
+        st = cn.prepareStatement(sql);
+
+        st.setString(1, p.getPid());
+        st.executeUpdate();
+
+      }catch(Exception e){
+      e.printStackTrace();
+      }finally{
+
+    }
+
+      AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
+
+  		ProductsDao dao = factory.getProductsDao();
+
+  		ocm.commit();
+  		ocm.closeConnection();
+
+        resc.setTarget("start");
 
         return resc;
     }
